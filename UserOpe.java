c3ca6f4d554e249c5d7e4ff856378a276829f159
EvenFuu <44134046+EvenFuu@users.noreply.github.com>
2019-01-12 22:05:38 +0800

    uope

---
+++ UserOpe.java
@@ -0,0 +1,18 @@
+package com.model;
+
+public class UserOpe {
+	private String id;//登录名
+	private String login;//登录账号
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+}
