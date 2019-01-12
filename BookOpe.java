84c31a47e548d3059465a0524136857bf12b5ea8
EvenFuu <44134046+EvenFuu@users.noreply.github.com>
2019-01-12 21:55:32 +0800

    bope

---
+++ BookOpe.java
@@ -0,0 +1,20 @@
+package com.model;
+
+public class BookOpe {
+	private String ISBN; // 图书isbn号
+	private String id;//登录名
+	public String getISBN() 
+	{
+		return ISBN;
+	}
+	public void setISBN(String ISBN) 
+	{
+		this.ISBN = ISBN;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
